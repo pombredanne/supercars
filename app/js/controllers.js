436ddf06ef98c1656af14b0b119b1a5ef6351dc1
Mark Fink <mark@mark-fink.de>
2012-05-20 16:53:49 +0200

    now the cellar app is using the rest webservice

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,39 +3,11 @@
 /* Controllers */
 
 
-//function WineListCtrl() {}
 function WineListCtrl($scope, Wine) {
     $scope.wines = Wine.get_all();
-    // $scope.wines = [
-    //     {"id":"9","name":"BLOCK NINE"},
-    //     {"id":"11","name":"BODEGA LURTON"},
-    //     {"id":"1","name":"CHATEAU DE SAINT COSME"},
-    //     {"id":"7","name":"CHATEAU LE DOYENNE"},
-    //     {"id":"8","name":"DOMAINE DU BOUSCAT"},
-    //     {"id":"10","name":"DOMAINE SERENE"},
-    //     {"id":"2","name":"LAN RIOJA CRIANZA"},
-    //     {"id":"12","name":"LES MORIZOTTES"},
-    //     {"id":"3","name":"MARGERUM SYBARITE"},
-    //     {"id":"4","name":"OWEN ROE \"EX UMBRIS\""},
-    //     {"id":"5","name":"REX HILL"},
-    //     {"id":"6","name":"VITICCIO CLASSICO RISERVA"}
-    // ];
 }
 
 
-
-
-function WineDetailsCtrl($scope, Wine) {
-    $scope.wine = {
-"id":"9","name":"BLOCK NINE"
-,"year":"2009","grapes":"Pinot Noir","country":"USA","region":"California","description":"With hints of ginger and spice, this wine makes an excellent complement to light appetizer and dessert fare for a holiday gathering.","picture":"block_nine.jpg"};
-    //$scope.wine = Wine.get();
-    //
-    //$scope.wine = Wine.get({wineId: 'wine'});
-
-    //$scope.wine = Wine.get({wineId: $routeParams.wineId}, function(wine) {
-    // $scope.wine = Wine.query();
-    //$scope.wine = Wine.get({wineId: 'wine'}, function(wine) {
-        //$scope.mainImageUrl = phone.images[0];
-    //});
+function WineDetailsCtrl($scope, $routeParams, Wine) {
+    $scope.wine = Wine.get({id: $routeParams.wineId});
 }
--- app/js/services.js
+++ app/js/services.js
@@ -2,43 +2,10 @@
 
 /* Services */
 
-// angular.module('cellar.services', []).
-//   value('version', '0.1');
-
 
 angular.module('cellar.services', ['ngResource']).
     factory('Wine', function($resource) {
-        return $resource('http://localhost:port/cellar/wines/:id', 
-            {'port': ':27080'},
+        return $resource('/rest/cellar/wines/:id', {},
             {get_all: {method: 'GET', params:{'id': ''}, isArray:true}
         });
     });
-
-
-/*
-
-
-
-angular.module('phonecatServices', ['ngResource']).
-    factory('Phone', function($resource){
-  return $resource('phones/:phoneId.json', {}, {
-    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-  });
-});
-
-
-/* 
-angular.module('cellar.services', ['ngResource']).
-    factory('Wine', function($resource) {
-        return $resource('http://localhost:27080/cellar/wines/_find?criteria=%7B%22_id%22%3A%7B%22%24oid%22%3A%224fb784bcb0ab582e8e000000%22%7D%7D', {}, {
-            get: {method: 'GET', params:{}, isArray:false}
-        });
-    });
-
-angular.module('phonecatServices', ['ngResource']).
-    factory('Phone', function($resource){
-  return $resource('phones/:phoneId.json', {}, {
-    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-  });
-});
-*/
