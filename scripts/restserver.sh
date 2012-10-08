83a620ed5de38305cc735db5cf36926e002e1167
Mark Fink <mark@mark-fink.de>
2012-10-08 21:56:15 +0200

    fixed a path problem

--- scripts/restserver.sh
+++ scripts/restserver.sh
@@ -9,21 +9,21 @@
 # VIEW:
 #   http://localhost:8000
 
-#cd /usr/local/system/projects/entos/external/workspace 
+BASE_DIR=`dirname $0`
 
 case $1 in
     "start" )
         echo "start python .restserver"
-        nohup python ./backend/restserver.py > /tmp/nohup.out 2> /tmp/nohup.out &
+        nohup python $BASE_DIR/../backend/restserver.py > /tmp/nohup.out 2> /tmp/nohup.out &
         ;;
     "stop" )
         echo "stop python restserver"
-        kill `ps aux | grep "python ./backend/restserver.py" | grep -v grep | awk '{print $2}'` > /dev/null
+        kill `ps aux | grep "python $BASE_DIR/../backend/restserver.py" | grep -v grep | awk '{print $2}'` > /dev/null
         ;;
     "restart" )
         echo "restart python restserver"
-        kill `ps aux | grep "python ./backend/restserver.py" | grep -v grep | awk '{print $2}'` > /dev/null 
-        nohup python ./backend/restserver.py > /tmp/nohup.out 2> /tmp/nohup.out &
+        kill `ps aux | grep "python $BASE_DIR/../backend/restserver.py" | grep -v grep | awk '{print $2}'` > /dev/null 
+        nohup python $BASE_DIR/../backend/restserver.py > /tmp/nohup.out 2> /tmp/nohup.out &
         ;;
     *)
         echo "need start|stop|restart"
